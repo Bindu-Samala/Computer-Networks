775330a4cdaa6e1f751f10b3db4622864600c624
Bindu-Samala <65999998+Bindu-Samala@users.noreply.github.com>
2024-04-25 22:55:34 +0530

    Added code for UDPServer.java

---
+++ UDP/UDPServer.java
@@ -0,0 +1,54 @@
+package com.udpclientserver;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPServer {
+  
+  public static class ServerThread implements Runnable {
+    DatagramSocket socket = null;
+
+    public ServerThread(int port) throws Exception {
+      socket = new DatagramSocket(port);
+    }
+
+    @Override
+    public void run() {
+      while (true) {
+        try {
+          byte[] buf = new byte[1024];
+          DatagramPacket packet = new DatagramPacket(buf, buf.length);
+          System.out.println("Ready.");
+          socket.receive(packet);
+
+          String received = new String(packet.getData(), 0, packet.getLength());
+          System.out.println("[Server] <RECV> " + received);
+          String reversed = new StringBuilder(received).reverse().toString();
+          buf = reversed.getBytes();
+          System.out.println("[Server] <SEND> " + reversed);
+          
+          InetAddress address = packet.getAddress();
+          int port = packet.getPort();
+
+          packet = new DatagramPacket(buf, buf.length, address, port);
+          socket.send(packet);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    /*
+    if (args.length != 1) {
+      System.err.println("Usage: java Server <port>");
+      return;
+    }
+    int port = Integer.parseInt(args[0]);
+    /* Uncomment to get port from CLI. */
+    int port = 12345;
+    new Thread(new ServerThread(port)).start();
+  }
+}
